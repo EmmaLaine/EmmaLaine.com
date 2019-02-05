aa5e84d30cc3818d6ad1e212306eaa2b8eeb4df4
Emma <EmmaLaine@hotmail.com>
2019-02-05 15:11:47 -0600

    Delete fancies.js

--- marine/images/fancies.js
+++
@@ -1,9 +0,0 @@
-//responsive topnav
-	function responsiveNav() {
-		var x = document.getElementById("myTopnav");
-		if (x.className === "topnav") {
-			x.className += " responsive";
-		} else {
-			x.className = "topnav";
-		}
-	};
