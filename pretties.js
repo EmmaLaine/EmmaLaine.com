7642f4fb66a857bbc750033d7303f4616dbf1d06
Emma <EmmaLaine@hotmail.com>
2019-02-06 09:24:23 -0600

    Delete pretties.js

--- pretties.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "top-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-nav";
-    }
-};
-
